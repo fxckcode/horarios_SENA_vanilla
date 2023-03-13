d20749f3c864144ecef842df14cdfe070a78e46e
TheBlowers <cristianbenitezg06@gmail.com>
2023-03-13 16:18:48 -0500

    Add create Fichas, add modal

---
+++ js/index.js
@@ -0,0 +1,16 @@
+var arrow = document.querySelectorAll(".arrow");
+var sidebar = document.querySelector(".sidebar");
+var sidebarBtn = document.querySelector(".bx-menu");
+for (var i = 0; i < arrow.length; i++) {
+    arrow[i].addEventListener("click", (e) => {
+        let arrowParent = e.target.parentElement.parentElement;
+        arrowParent.classList.toggle("showMenu");
+    });
+};
+sidebarBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+});
+
+if (sidebar.classList.toggle("close")) {
+    sidebar.classList.remove("close")
+}
